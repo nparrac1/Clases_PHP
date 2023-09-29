800d16fe4146c34177257573be8ef9984d8625f9
nparrac1 <npcruz630@gmail.com>
2023-09-29 11:54:32 -0300

    Clase-29/09/2023
    INSERT PHP,  CONSULTA
    CRUD
    CREATE, READ, UPDATE, DELETE

---
+++ guardar.php
@@ -0,0 +1,19 @@
+<?php
+
+$conexion = mysqli_connect("localhost","root", "", "repuestos" );
+
+$nombre_recibido = $_POST["nombre"];
+$origen_recibido = $_POST["origen"];
+$logo_recibido = $_POST["logo"];
+
+echo $nombre_recibido;
+echo $origen_recibido;
+echo $logo_recibido;
+
+$insert = "INSERT INTO marcas (nombre, origen, logo) VALUES ('$nombre_recibido','$origen_recibido','$logo_recibido')";
+$resultado = mysqli_query($conexion, $insert);
+
+header("Location: index.php")
+
+
+?>
--- index.php
+++ index.php
@@ -1,26 +1,83 @@
 <?php
-  $conexion = mysqli_connect("localhost","root", "", "alumnos" )
+  $conexion = mysqli_connect("localhost","root", "", "repuestos" );
+  $consulta = "SELECT * FROM marcas";
+  $resultado = mysqli_query($conexion, $consulta);
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-       $consulta = "SELECT * FROM estudiantes";
-       $resultado = mysqli_query($conexion, $consulta);
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Clase PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    
+    <div class="container p-5">
+      <div class="row">
+        <div class="col">
+        <form action="guardar.php" method = "POST">
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="basic-addon1">Nombre</span>
+          <input type="text" name="nombre" class="form-control" placeholder="Peugot">
+        </div>
 
-       while($row = mysqli_fetch_assoc($resultado)){
+        <div class="input-group mb-3">
+          <select class="form-select" name="origen">
+              <option selected>Seleccionar Origen</option>
+              <option value="japon">Japon</option>
+              <option value="china">China</option>
+              <option value="francia">Francia</option>
+          </select>
+        </div>
 
-        $nombre = $row['nombre'];
-        $apellido = $row['apellido'];
-        $rut = $row['rut'];
-        echo "<h1>".$nombre." ".$apellido." - ".$rut."";
+
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="basic-addon1">Logo</span>
+          <input type="text" name="logo" class="form-control" placeholder="../logo-peugot.jpg">
+        </div>
+
+        <input type="submit" value="Guardar" class="btn btn-primary">
+        </form>
+        </div>
+        <div class="col">
+        <table class="table">
+     <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col"> Nombre</th>
+        <th scope="col">Origen</th>
+        <th scope="col">Imagen</th>
+        <th scope="col">Opciones</th>
+      </tr>
+    </thead>
+
+    <tbody>
+
+      <?php
+      
+
+        while($row = mysqli_fetch_assoc($resultado)){
+
+          echo "<tr>";
+          echo "<td>".$row["id"]."</td>";
+          echo "<td>".$row["nombre"]."</td>";
+          echo "<td>".$row["origen"]."</td>";
+          echo "<td>".$row["logo"]."</td>";
+          echo "<td></td>";
+          echo "</tr>";
+          
        }
-    ?>
-</body>
+      ?>
+
+    </tbody>
+    </table>
+        </div>
+      </div>
+    </div>
+    
+    
+  </body>
 </html>
